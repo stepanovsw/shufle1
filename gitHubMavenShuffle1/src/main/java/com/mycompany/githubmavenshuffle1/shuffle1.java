d7042b5dec6b9c2a61ddda2058770d2472050206
user <user@dell>
2018-03-26 19:27:18 +0300

    все блоки добавил

---
+++ gitHubMavenShuffle1/src/main/java/com/mycompany/githubmavenshuffle1/shuffle1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.githubmavenshuffle1;
+
+/**
+ *
+ * @author user
+ */
+public class shuffle1 {
+    public static void main(String[] args) {
+        int x =3;
+        while (x>0) {
+                       
+        }
+        //1 блок
+        if (x==1){
+        System.out.print("d");
+        }
+        
+        //2 блок
+        if (x==2){
+            System.out.print("b c");
+        }
+        
+        //3 блок
+        if (x>2){
+        System.out.print("a");
+        }
+        
+        //4 блок
+        x = x-1;
+        System.out.print("-");
+    }
+    
+}
